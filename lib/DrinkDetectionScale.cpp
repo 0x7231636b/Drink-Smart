5815364564cfe1a9dd8c735af14fdca84ec7c447
Markus Hallmann <markus@hallmann.pw>
2024-03-18 18:41:34 +0100

    Created first approach of DrinkDetectionScale

---
+++ lib/DrinkDetectionScale.cpp
@@ -0,0 +1,21 @@
+#include "DrinkDetectionScale.hpp"
+
+#include <HX711.h>
+
+DrinkDetectionScale::DrinkDetectionScale(const int &calibrationFactor,
+                                         const int &doutPin, const int &sckPin)
+    : lastMeasuredValue(0), scale(HX711()) {
+  scale.begin(doutPin, sckPin);
+  scale.set_scale(calibrationFactor);
+
+  // TODO: Implement a std::thread for the scale to measure the weight all the
+  // time Think about a delay that doesn't push the cpu to the limit
+  // Thread function within this implementation started by the constructor
+}
+
+void DrinkDetectionScale::setRequestSenderCallback(
+    std::function<void(const long &)> callback) {
+  requestSender = callback;
+}
+
+void DrinkDetectionScale::tare() { scale.tare(); }
---
+++ lib/DrinkDetectionScale.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <functional>
+
+class DrinkDetectionScale {
+
+public:
+  DrinkDetectionScale(const int &calibrationFactor, const int &doutPin = 33,
+                      const int &sckPin = 32);
+  virtual ~DrinkDetectionScale() = default;
+
+  // TODO: research whether std::function is a reference or ptr by default, pass
+  // this as reference or ptr if needed
+  void setRequestSenderCallback(std::function<void(const long &)> callback);
+
+  void tare();
+
+private:
+  HX711 scale;
+  long lastMeasuredValue;
+
+  std::function<void(const long &)> requestSender;
+};
